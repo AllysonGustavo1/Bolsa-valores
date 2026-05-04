97d1128a7909c56d4d66326f480cda698d5f4273
franklin.lopes.ext <franklin.lopes.ext@synchro.com.br>
2026-05-04 09:43:04 -0300

    fix: arquivo Bolsa_Contexto.h com novas definições de tipos

--- Bolsa_Contexto.h
+++ Bolsa_Contexto.h
@@ -1,65 +1,36 @@
-#ifndef _Bolsa_Contexto_h
-#define _Bolsa_Contexto_h
+MACHINE Bolsa_Contexto
 
-#include <stdint.h>
-#include <stdbool.h>
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-#ifndef Bolsa_Contexto__ERRO
-#define Bolsa_Contexto__ERRO 0
-#endif
+SETS
+  TIPO_LADO = {lado_compra, lado_venda};
+  STATUS_ORDEM = {status_livre, status_aberta, status_cancelada, status_executada}
 
-#ifndef Bolsa_Contexto__OK
-#define Bolsa_Contexto__OK 1
-#endif
+CONCRETE_CONSTANTS
+  MAX_CLIENTES,
+  MAX_ATIVOS,
+  MAX_ORDENS,
+  MAX_DINHEIRO,
+  MAX_QUANTIDADE,
+  MAX_PRECO,
+  ERRO,
+  OK
 
-/* Clause SETS */
-typedef enum
-{
-    Bolsa_Contexto__lado_compra,
-    Bolsa_Contexto__lado_venda
-    
-} Bolsa_Contexto__TIPO_LADO;
-#define Bolsa_Contexto__TIPO_LADO__max 2
-typedef enum
-{
-    Bolsa_Contexto__status_livre,
-    Bolsa_Contexto__status_aberta,
-    Bolsa_Contexto__status_cancelada,
-    Bolsa_Contexto__status_executada
-    
-} Bolsa_Contexto__STATUS_ORDEM;
-#define Bolsa_Contexto__STATUS_ORDEM__max 4
+PROPERTIES
+  MAX_CLIENTES : NAT1 &
+  MAX_ATIVOS : NAT1 &
+  MAX_ORDENS : NAT1 &
+  MAX_DINHEIRO : NAT1 &
+  MAX_QUANTIDADE : NAT1 &
+  MAX_PRECO : NAT1 &
+  ERRO : NAT &
+  OK : NAT1 &
 
-/* Clause CONCRETE_CONSTANTS */
-/* Basic constants */
-/* TO DO: #define Bolsa_Contexto__MAX_CLIENTES */
-/* TO DO: #define Bolsa_Contexto__MAX_ATIVOS */
-/* TO DO: #define Bolsa_Contexto__MAX_ORDENS */
-/* TO DO: #define Bolsa_Contexto__MAX_DINHEIRO */
-/* TO DO: #define Bolsa_Contexto__MAX_QUANTIDADE */
-/* TO DO: #define Bolsa_Contexto__MAX_PRECO */
-/* TO DO: #define Bolsa_Contexto__ERRO */
-/* TO DO: #define Bolsa_Contexto__OK */
-/* Array and record constants */
+  MAX_CLIENTES = 20 &
+  MAX_ATIVOS = 10 &
+  MAX_ORDENS = 50 &
+  MAX_DINHEIRO = 1000000 &
+  MAX_QUANTIDADE = 1000 &
+  MAX_PRECO = 1000 &
+  ERRO = 0 &
+  OK = 1
 
-
-
-
-
-
-
-
-
-/* Clause CONCRETE_VARIABLES */
-
-extern void Bolsa_Contexto__INITIALISATION(void);
-
-
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
-
-
-#endif /* _Bolsa_Contexto_h */
+END
